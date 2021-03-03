f49cd74e0b1f2e8cb0631e25b940b4028619ad31
Victor Rocha <victor.barbosa.18@outlook.com>
2021-03-03 17:29:15 -0400

    reworks

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 80
 const path = require('path')
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
